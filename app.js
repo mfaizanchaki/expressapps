247024008f6b6f597f3ac8daff0dd106f8b4b075
mfaizanchaki <faizanchaki.mfc@gmail.com>
2016-08-24 10:14:32 +0500

    Added jade template settings and authentication with passport-local module

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
 app.get('/', function(req, res, next){
 	res.send('Assalamu alaikum');
+});
+
+app.get('/', function(req, res, next){
+	res.render(index);
 });
 
 app.listen(3000);
